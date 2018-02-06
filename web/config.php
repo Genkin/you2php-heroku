0d6c46875fb595bbbc10b1d1e146441b83c79fad
Genkin <lyj@live.hk>
2018-02-06 12:51:13 +0900

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('APIKEY', 'AIzaSyA7jZpKAemgKwmI2C_iUl7EpGkoLiul6JY');//这里改成您的api key
+define('GJ_CODE', 'US');//国家代码
+define('SITE_NAME', 'Everyvid2');//网站名字
+define('TITLENAME', 'Everyvid2');//网站名字
+define('EN2DEKEY', '$%WS/#!xW36Y$em_eZz]:|<');//一个随机字符串，加解密KEY
+define('EMAIL', 'lyj@live.hk');//邮箱
 ?>
